1a92650d3eedb958997b532a6dc0bef610f1b55a
ataman <atamanenkoalex90@gmail.com>
2023-04-22 16:21:28 +0300

    Create generate views

--- university/student/models.py
+++ university/student/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    age = models.PositiveIntegerField(validators=[MaxValueValidator(120)])
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- university/student/views.py
+++ university/student/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.http import HttpResponse
+from django.utils.datastructures import MultiValueDictKeyError
+from faker import Faker
+
+from .models import Student
+
+
+def index(request):
+    return HttpResponse("Students index page.")
+
+
+def generate_student(request):
+    fake = Faker()
+    student = Student.objects.create(first_name=fake.first_name(),
+                                     last_name=fake.last_name(),
+                                     age=random.randint(16, 60))
+    return HttpResponse(f"Generate one fake student: {student}")
+
+
+def generate_students(request):
+    try:
+        if request.GET["count"].isdigit() and 0 < int(request.GET["count"]) <= 100:
+            count = int(request.GET["count"])
+            fake = Faker()
+            students_list = []
+            for _ in range(count):
+                students_list.append(Student(first_name=fake.first_name(),
+                                             last_name=fake.last_name(),
+                                             age=random.randint(16, 60)))
+            students = Student.objects.bulk_create(students_list)
+            return HttpResponse(f"Generate {count} fake students: <p>{students}</p>")
+        else:
+            count = request.GET["count"]
+            return HttpResponse(f"Count {count} is not valid! Try again")
+    except MultiValueDictKeyError:
+        return HttpResponse('Query parameter "count" did not find! Try again')
